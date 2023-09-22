994b6b9a421f135aca2199bb89063daa6ad5696b
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-22 13:42:52 +0530

    created a SAP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/header";
+import Home from "./components/Home/home";
+import About from "./components/About/about";
+import Contact from "./components/Contact/contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Home />
+    <About />
+    <Contact />
+  </>
+);
 
 export default App;
