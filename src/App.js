dff0d7a6007690494fea7ad9c879d0b0f8309a42
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-22 14:03:22 +0530

    added Link components in the Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/header";
 import Home from "./components/Home/home";
 import About from "./components/About/about";
 import Contact from "./components/Contact/contact";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
     <Home />
     <About />
     <Contact />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
