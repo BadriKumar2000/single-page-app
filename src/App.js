522239a1be1f1c0a87701a107abacd070bd292a8
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-22 16:20:40 +0530

    User can toggle btw the pages using react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/header";
 import Home from "./components/Home/home";
 import About from "./components/About/about";
 import Contact from "./components/Contact/contact";
+import NotFound from "./components/NotFound/notfound";
 
 const App = () => (
   <BrowserRouter>
     <Header />
-    <Home />
-    <About />
-    <Contact />
+    <Routes>
+      <Route exact path="/" Component={Home} />
+      <Route exact path="/about" Component={About} />
+      <Route exact path="/contact" Component={Contact} />
+      <Route exact path="/*" Component={NotFound} />
+    </Routes>
   </BrowserRouter>
 );
 
